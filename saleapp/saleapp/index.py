216dd17419a5b61413cfc2a842f9c4795ef04188
DuongBinh <binh1082003@gmail.com>
2024-03-16 14:42:58 +0700

    2:42

---
+++ saleapp/saleapp/index.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import dao
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    cates = dao.load_categories()
+    prods = dao.load_products();
+
+    return render_template('index.html', categories=cates, products=prods)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        app.run(debug=True)
+
