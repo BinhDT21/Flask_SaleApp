c43ddfa050d11bfae4bbda5cbc88b3cdc211e975
DuongBinh <binh1082003@gmail.com>
2024-03-16 16:17:54 +0700

    4:17

--- saleapp/saleapp/index.py
+++ saleapp/saleapp/index.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import dao
 
-
 app = Flask(__name__)
 
 
@@ -10,13 +9,22 @@ def index():
     kw = request.args.get("kw")
     cate_id = request.args.get("category_id")
 
-    cates = dao.load_categories()
+
     prods = dao.load_products(kw=kw, cate_id=cate_id);
 
-    return render_template('index.html', categories=cates, products=prods)
+    return render_template('index.html', products=prods)
 
+
+@app.route('/login')
+def process_login_user():
+    return render_template('login.html')
+
+@app.context_processor
+def commont_attributes():
+    return {
+        'categories': dao.load_categories()
+    }
 
 if __name__ == '__main__':
     with app.app_context():
         app.run(debug=True)
-
