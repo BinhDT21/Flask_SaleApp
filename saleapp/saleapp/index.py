45c1117bd1d31647d60cf641935a0f7dbc05d00a
DuongBinh <binh1082003@gmail.com>
2024-03-16 15:23:22 +0700

    3:22

--- saleapp/saleapp/index.py
+++ saleapp/saleapp/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import dao
 
 
@@ -7,8 +7,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    kw = request.args.get("kw")
+    cate_id = request.args.get("category_id")
+
     cates = dao.load_categories()
-    prods = dao.load_products();
+    prods = dao.load_products(kw=kw, cate_id=cate_id);
 
     return render_template('index.html', categories=cates, products=prods)
 
