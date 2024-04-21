cc0e6d1ce7027121d55ba046338f923ea7e55039
duong thanh binh <binh1082003@gmail.com>
2024-04-21 19:53:46 +0700

    add floating labels

--- saleapp/saleapp/index.py
+++ saleapp/saleapp/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import dao
 
 app = Flask(__name__)
@@ -9,21 +9,32 @@ def index():
     kw = request.args.get("kw")
     cate_id = request.args.get("category_id")
 
-
-    prods = dao.load_products(kw=kw, cate_id=cate_id);
+    prods = dao.load_products(kw=kw, cate_id=cate_id)
 
     return render_template('index.html', products=prods)
 
 
-@app.route('/login')
+@app.route('/login', methods=['get', 'post'])
 def process_login_user():
-    return render_template('login.html')
+    err_msg = ''
+    if request.method.__eq__('POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username.__eq__('admin') and password.__eq__('123'):
+            return redirect('/')
+        else:
+            err_msg = 'Tên đăng nhập hoặc mật khẩu không hợp lệ'
+
+    return render_template('login.html', err_msg=err_msg)
+
 
 @app.context_processor
-def commont_attributes():
+def common_attributes():
     return {
         'categories': dao.load_categories()
     }
+
 
 if __name__ == '__main__':
     with app.app_context():
